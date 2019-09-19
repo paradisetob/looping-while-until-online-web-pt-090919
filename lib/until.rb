006047100d8a6a4ecd254c91e8e4ff545c48b885
tracey brimage <onelovenilynn@gmail.com>
2019-09-19 20:38:11 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,15 @@
 def using_until
-  levitation_force = 6
-  until counter +< 10
-    
+  
+  until levitation_force = 6
+    puts'Wingardium Leviosa'
+    levitation_force += 1
+    puts 'Wingardium Leviosa'
+    levitation_force += 1
+    puts'Wingardium Leviosa'
+    levitation_force += 1
+    puts'Wingardium Leviosa'
+    levitation_force += 1
+
 
 
 end
