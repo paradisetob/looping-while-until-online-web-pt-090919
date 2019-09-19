ee9ee78c9d7e34fbce66cd40f734ab0ff8fed1a7
tracey brimage <onelovenilynn@gmail.com>
2019-09-19 20:26:44 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,7 +2,12 @@ def using_while
 
   while levitation_force = 6
     puts'Wingardium Leviosa'
-    
-
+    levitation_force += 1
+    puts 'Wingardium Leviosa'
+    levitation_force += 1
+    puts'Wingardium Leviosa'
+    levitation_force += 1
+    puts'Wingardium Leviosa'
+    levitation_force += 1
   end
 end
