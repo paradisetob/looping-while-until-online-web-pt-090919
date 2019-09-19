57b42a2c59c9752301f69adb17062a528a178262
tracey brimage <onelovenilynn@gmail.com>
2019-09-19 20:38:32 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,15 @@
 def using_until
-  levitation_force = 6
-  #your code here
-    
-end
+  
+  until levitation_force = 6
+    puts'Wingardium Leviosa'
+    levitation_force += 1
+    puts 'Wingardium Leviosa'
+    levitation_force += 1
+    puts'Wingardium Leviosa'
+    levitation_force += 1
+    puts'Wingardium Leviosa'
+    levitation_force += 1
 
+
+
+end
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,13 @@
 def using_while
-  levitation_force = 6
-	
-  #your code here
+
+  while levitation_force = 6
+    puts'Wingardium Leviosa'
+    levitation_force += 1
+    puts 'Wingardium Leviosa'
+    levitation_force += 1
+    puts'Wingardium Leviosa'
+    levitation_force += 1
+    puts'Wingardium Leviosa'
+    levitation_force += 1
+  end
 end
-
-
